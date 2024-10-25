ddf986f931c3a8c5fe3d845b55176e60729b53c6
Fuad Godzhaev <72225859+fuad-godzhaev@users.noreply.github.com>
2024-10-25 15:58:54 +0100

    Quaternion, slerp

--- games-engines-2024/enemy.gd
+++ games-engines-2024/enemy.gd
@@ -7,17 +7,13 @@ var forw
 var axis
 var theta
 
+var q1:Quaternion
+var q2:Quaternion
+
+var t
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	
-	
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	
-	
 	to_player = tank.global_position - global_position	
 	to_player = to_player.normalized()
 	forw = global_transform.basis.z
@@ -27,14 +23,33 @@ func _process(delta: float) -> void:
 	DebugDraw2D.set_text("forw ", forw)
 	
 	axis = to_player.cross(forw)
-	
 	theta = acos(to_player.dot(forw))
+	
+	q2 = Quaternion(axis, theta)
+	q1 = global_basis.get_rotation_quaternion()
+	t = 0
 	
 	DebugDraw2D.set_text("angle to player", rad_to_deg(theta))
 	
 	DebugDraw2D.set_text("axis", axis)
 	
 	rotation = Vector3(0, theta, 0)
+	
+	pass # Replace with function body.
 
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	
+	var q:Quaternion
+	
+	if (t < 1.0):
+		t = t + delta
+	else:
+		t = 1.0
+		
+	DebugDraw2D.set_text("t", t)
+	q = q1.slerp(q2, t)
+	global_basis = Basis(q)
 
 	pass
