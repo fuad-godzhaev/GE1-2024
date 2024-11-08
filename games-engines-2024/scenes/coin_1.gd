be47df9e206e5f920eec0a2a2e052a13ee107cda
Fuad Godzhaev <72225859+fuad-godzhaev@users.noreply.github.com>
2024-11-08 16:17:25 +0000

    Coin drop

--- games-engines-2024/scenes/coin_1.gd
+++ games-engines-2024/scenes/coin_1.gd
@@ -9,9 +9,17 @@ extends CSGCylinder3D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	
+	var desired = sqrt(global_position.y / (.5 * abs(gravity.y)))
+	print(desired)
+	
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if global_position.y > 0:
+		time += delta
+		velocity += gravity * delta
+		global_position += velocity * delta
+	
 	pass
